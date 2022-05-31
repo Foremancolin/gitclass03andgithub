d86522286e9020063198385c969a286b38b46dea
Foremancolin <colinf2399@gmail.com>
2022-05-31 19:54:25 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -2,4 +2,11 @@ package github;
 
 public class gitAndGithub {
 
-}
+	public static void main(String[] args) {
+		System.out.print("today we learn github");
+		System.out.print("today is a very good day");
+		}
+	}
+	
+	
+
